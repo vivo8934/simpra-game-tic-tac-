30847997bdd789e12e3ebeff973afa3849c6ecb9
Ngqanekana <mfundo.v.ngqanekana@accenture.com>
2020-04-28 12:45:32 +0200

    fully functional game

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 "# simpra-game-tic-tac-" 
+
+## this is a fully functional game using only front-end (HTML5, CSS3 And Javascript)
+
+#Author  : Simphiwe kaba(CapaCity Student)
---
+++ script.js
@@ -0,0 +1,22 @@
+function handleCellClick(clickedCellEvent) {
+    const clickedCell = clickedCellEvent.target;
+    const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
+
+    if (gameState[clickedCellIndex] !== "" || !gameActive) {
+        return;
+    }
+
+    handleCellPlayed(clickedCell, clickedCellIndex);
+    handleResultValidation();
+}
+
+function handleRestartGame() {
+    gameActive = true;
+    currentPlayer = "X";
+    gameState = ["", "", "", "", "", "", "", "", ""];
+    statusDisplay.innerHTML = currentPlayerTurn();
+    document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = "");
+}
+
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
+document.querySelector('.game--restart').addEventListener('click', handleRestartGame);
